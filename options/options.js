f23151229097c50743159bcc05aafc3238683472
Crowdbotics <team@crowdbotics.com>
2023-08-03 08:38:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tight-union-42610",
+  url: "https://tight-union-42610.botics.co",
+  api: "https://tight-union-42610.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
